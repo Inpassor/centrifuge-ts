458d94ebc55fabc1b2c9ed1878fe33f32055aa21
Oleg Kuzmin <oleg.kuzmin@arcadia.spb.ru>
2018-02-28 10:51:11 +0300

    Work in progress
    Signed-off-by: Oleg Kuzmin <oleg.kuzmin@arcadia.spb.ru>

--- make-proto.cmd
+++ make-proto.cmd
@@ -1,14 +1,15 @@
 @echo off
 @setlocal
 set CUR_DIR=%cd%
-cd %~dp0
+set PROJECT_DIR=%~dp0
+cd %PROJECT_DIR%
 if exist node_modules\centrifuge (
     cd node_modules\centrifuge
     git pull
 ) else (
     git clone https://github.com/centrifugal/centrifuge.git -b master node_modules\centrifuge
 )
-cd %~dp0
+cd %PROJECT_DIR%
 call node_modules\.bin\pbjs ./node_modules/centrifuge/misc/proto/client.proto -t static-module -o ./src/Proto.js
 call node_modules\.bin\pbts -o ./src/Proto.d.ts ./src/Proto.js
 cd %CUR_DIR%
