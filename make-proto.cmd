67c16de3e797f3f4d5996dcc466902f7f8ce4902
Oleg Kuzmin <oleg.kuzmin@arcadia.spb.ru>
2018-02-28 10:49:14 +0300

    Work in progress
    Signed-off-by: Oleg Kuzmin <oleg.kuzmin@arcadia.spb.ru>

--- make-proto.cmd
+++ make-proto.cmd
@@ -8,5 +8,8 @@ if exist node_modules\centrifuge (
 ) else (
     git clone https://github.com/centrifugal/centrifuge.git -b master node_modules\centrifuge
 )
+cd %~dp0
+call node_modules\.bin\pbjs ./node_modules/centrifuge/misc/proto/client.proto -t static-module -o ./src/Proto.js
+call node_modules\.bin\pbts -o ./src/Proto.d.ts ./src/Proto.js
 cd %CUR_DIR%
 @endlocal
